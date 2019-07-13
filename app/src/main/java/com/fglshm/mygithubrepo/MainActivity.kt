8917f3019016849d46a3df221aa6e7238609f043
前野翔平 <fglshm@gmail.com>
2019-07-13 18:08:39 +0900

    commi

--- app/src/main/java/com/fglshm/mygithubrepo/MainActivity.kt
+++ app/src/main/java/com/fglshm/mygithubrepo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.fglshm.mygithubrepo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fglshm.extensions2.showLogD
 
 class MainActivity : AppCompatActivity() {
+
+    private val cls = MainActivity::class
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cls.showLogD("Hello")
     }
 }
